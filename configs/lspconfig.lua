0d9c574e60352b6cb6bba53d77dbb0e4d059133b
David Pech <dpech@chemaxon.com>
2023-04-19 23:20:27 +0200

    Added docker lsp config

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,60 +3,68 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-lspconfig.rust_analyzer.setup({
+lspconfig.rust_analyzer.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"rust"},
-  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
-})
+  filetypes = { "rust" },
+  root_dir = lspconfig.util.root_pattern "Cargo.toml",
+}
 
-lspconfig.terraformls.setup({
+lspconfig.terraformls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = {"terraform-ls", "serve"},
-  filetypes = {"terraform", "terraform-vars"},
+  cmd = { "terraform-ls", "serve" },
+  filetypes = { "terraform", "terraform-vars" },
   root_dir = lspconfig.util.root_pattern(".terraform", ".terraform.lock.hcl", ".git"),
-})
+}
 
-lspconfig.gopls.setup({
+lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = {"gopls"},
-  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
   root_dir = lspconfig.util.root_pattern("go.mod", ".git"),
-})
+}
 
-lspconfig.bashls.setup({
+lspconfig.bashls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = {"bash-language-server", "start"},
-  filetypes = {"sh"},
+  cmd = { "bash-language-server", "start" },
+  filetypes = { "sh" },
   root_dir = lspconfig.util.find_git_ancestor,
   settings = {
     bashIde = {
-      globPattern = "*@(.sh|.inc|.bash|.command)"
-    }
+      globPattern = "*@(.sh|.inc|.bash|.command)",
+    },
   },
   single_file_support = true,
-})
+}
 
-lspconfig.ansiblels.setup({
+lspconfig.ansiblels.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = {"ansible-language-server", "--stdio"},
-  filetypes = {"yaml.ansible"},
-})
+  cmd = { "ansible-language-server", "--stdio" },
+  filetypes = { "yaml.ansible" },
+}
 
-lspconfig.yamlls.setup({
+lspconfig.yamlls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = {"yaml-language-server", "--stdio"},
-  filetypes = {"yaml", "yaml.dockercompose"},
+  cmd = { "yaml-language-server", "--stdio" },
+  filetypes = { "yaml", "yaml.dockercompose" },
   settings = {
     redhat = {
       telemetry = {
-        enabled = false
-      }
-    }
+        enabled = false,
+      },
+    },
   },
-})
+}
+
+lspconfig.dockerls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "docker-langserver", "--stdio" },
+  filetypes = { "dockerfile" },
+  root_dir = lspconfig.util.root_pattern("Dockerfile"),
+}
