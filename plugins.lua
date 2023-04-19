2e6debe562bbf766ccee8bfdfb7b6fc00059e430
David Pech <dpech@chemaxon.com>
2023-04-19 22:30:25 +0200

    Adding leap for mouseless navigation

--- plugins.lua
+++ plugins.lua
@@ -101,5 +101,14 @@ local plugins = {
       }
     end,
   },
+  { "ggandor/leap.nvim",
+    lazy = false,
+    dependencies = {
+      "tpope/vim-repeat",
+    },
+    config = function ()
+      require("leap").add_default_mappings()
+    end,
+  },
 }
 return plugins
