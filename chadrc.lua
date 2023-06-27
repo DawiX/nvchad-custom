1bb6e30fa071d66b34aa3cc0cc2e55e421f75cde
David Pech <dpech@chemaxon.com>
2023-06-27 13:40:52 +0200

    Added light theme toggle and enabled nvdash

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,12 @@
 ---@type ChadrcConfig 
 local M = {}
-M.ui = {theme = 'gruvbox'}
+M.ui = {
+  theme = 'gruvbox',
+  theme_toggle = { "gruvbox", "gruvbox_light" },
+  nvdash = {
+      load_on_startup = true,
+  }
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
